d79c2630f8dbc50216cfb2f869bc6bfb54974dac
rangermauve <RangerMauve@hotmail.com>
2015-09-22 22:29:22 -0400

    Fix escaping regular text

--- test/testEscape.js
+++ test/testEscape.js
@@ -5,7 +5,7 @@ var builder = require("../lib/stringBuilder");
 var components = new Components(builder);
 
 var name = "test-component";
-var template = '<div class="{{@totally}}">{{@illegal}}</div>';
+var template = '<div class="{{@totally}}">{{@illegal}}</div> ><';
 var data = {
 	totally: '"&&  wooooo~~~"',
 	illegal: "<script>alert(1)</script>"
