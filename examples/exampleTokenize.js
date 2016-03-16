cb01a23af70bf302314cf257ebfd176c5bbb332b
RangerMauve <RangerMauve@htomail.com>
2016-03-15 22:57:34 -0400

    Render token example to something more meaningful

--- examples/exampleTokenize.js
+++ examples/exampleTokenize.js
@@ -7,11 +7,8 @@ var templateName = path.join(__dirname, "./example.html");
 var template = fs.readFileSync(templateName, "utf8");
 var inspect = require("util").inspect;
 
-tokenize(template).forEach(function (token) {
-	console.log(token.type);
-	if (token.name)
-		console.log("\tName:", inspect(token.name));
-	if (token.content)
-		console.log("\tContent:", inspect(token.content));
-	console.log("");
-});
+var tokens = tokenize(template);
+
+console.log(inspect(tokens, {
+	depth: null
+}));
